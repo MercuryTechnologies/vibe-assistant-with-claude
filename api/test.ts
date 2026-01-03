68e5d1167da28079ca02947fc5fd798805dec521
Ryan Wiggins <ryanw@mercury.com>
2026-01-03 15:18:22 -0800

    Remove test file, chat streaming is working

--- api/test.ts
+++
@@ -1,9 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default async function handler(
-  req: VercelRequest,
-  res: VercelResponse
-): Promise<void> {
-  res.status(200).json({ message: 'Hello from test!', method: req.method })
-}
-
